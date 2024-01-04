2339c3e902c6b5a4de5760cb36f8f48542797157
Михаил <misha13022008@gmail.com>
2024-01-04 18:21:06 +0300

    changed head logic

--- Assets/NamehaveCat/Scripts/Enemy/EnemyHead.cs
+++ Assets/NamehaveCat/Scripts/Enemy/EnemyHead.cs
@@ -7,15 +7,23 @@ namespace NamehaveCat.Scripts.Enemy
         [SerializeField] private float stunTime;
         [SerializeField] private float damageOnStun;
         [SerializeField] private string deathMessage;
-        private Enemy _enemy;
+        [SerializeField] private float damageWaitTime = 0.5f;
 
-        private void OnCollisionEnter2D(Collision2D other) => OnCollision(other.transform);
-        private void OnTriggerEnter2D(Collider2D other) => OnCollision(other.transform);
+        private Enemy _enemy;
+        private float _previousTime;
+
+        private void OnCollisionStay2D(Collision2D other) => OnCollision(other.transform);
+        private void OnTriggerStay2D(Collider2D other) => OnCollision(other.transform);
 
         private void OnCollision(Component other)
         {
             if (!other.TryGetComponent<PlayerPawsTag>(out _))
                 return;
+
+            if (_previousTime + damageWaitTime > Time.time)
+                return;
+
+            _previousTime = Time.time;
 
             if (_enemy.State == EnemyState.Waiting)
             {
