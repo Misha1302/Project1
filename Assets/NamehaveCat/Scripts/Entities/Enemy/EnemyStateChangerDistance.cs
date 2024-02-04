39139458a340983477b88fddc9a205e9cb11a58a
Михаил <misha13022008@gmail.com>
2024-02-04 23:22:28 +0300

    distance snowman fixed

--- Assets/NamehaveCat/Scripts/Entities/Enemy/EnemyStateChangerDistance.cs
+++ Assets/NamehaveCat/Scripts/Entities/Enemy/EnemyStateChangerDistance.cs
@@ -10,18 +10,31 @@
     {
         [SerializeField] private float distance = 5;
         [SerializeField] private float maxDegrees = 30;
+        [SerializeField] private float offset = -0.5f;
 
         public override EnemyState TryGetNewState(Direction dir)
         {
-            var a = transform.position;
+            var a = transform.position + Vector3.up * offset;
             var b = GameManager.Instance.PlayerController.transform.position;
 
             var rightDist = Math.Abs(a.x - b.x) <= distance;
             var rightAngle = a.Degrees(b) <= maxDegrees;
-            var rightDirection = dir.Has(Direction.Left) ? b.x < a.x : a.x < b.x;
+            var trueDirection = dir.Has(Direction.Left) ? b.x < a.x : a.x < b.x;
             var isPlayer = RaycastPlayer(b - a);
+            
+            DrawLines(a, b);
 
-            return rightDist && rightAngle && rightDirection && isPlayer ? EnemyState.Attack : EnemyState.Walk;
+            return rightDist && rightAngle && trueDirection && isPlayer ? EnemyState.Attack : EnemyState.Walk;
+        }
+
+        private static void DrawLines(Vector3 a, Vector3 b)
+        {
+            Debug.DrawLine(a, a + new Vector3(4, 1), Color.red);
+            Debug.DrawLine(a, a + new Vector3(4, -1), Color.red);
+            Debug.DrawLine(a, a + new Vector3(-4, 1), Color.red);
+            Debug.DrawLine(a, a + new Vector3(-4, -1), Color.red);
+            Debug.DrawLine(a, a + new Vector3(0.1f, 0.1f), Color.red);
+            Debug.DrawLine(b, b + new Vector3(0.1f, 0.1f), Color.red);
         }
 
         private bool RaycastPlayer(Vector2 dir)
